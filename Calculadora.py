fc7d1b873839e161bf94866e92ea85a6d3fbee2b
danydevtest <danydevtest@gmail.com>
2022-11-12 10:15:53 -0600

    Nuevo método en Calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -4,9 +4,14 @@ class Calculadora:
          self.valor2=int(input("Ingresa el valor 2: "))
      def Suma(self):
          return (self.valor1+self.valor2)
+     def Resta(self):
+         return (self.valor1-self.valor2)
+     
      def imprimir(self):
          print("La suma es: ",self.Suma())
+         print("La resta es: ",self.Resta())
         
-suma=Calculadora()
-suma.Suma()
-suma.imprimir()
+operaciones=Calculadora()
+operaciones.Suma()
+operaciones.Resta()
+operaciones.imprimir()
