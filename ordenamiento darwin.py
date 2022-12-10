e6071e3ec739f05dfd1154bd0a158be24a155b88
Darwin-Rosado <xander.rosado90@gmail.com>
2022-12-09 21:24:44 -0600

    ALGORITMO ORDEN DE SELECCIÓN DARWIN ROSADO

--- ordenamiento darwin.py
+++ ordenamiento darwin.py
@@ -2,7 +2,8 @@ def orden_seleccion(nums):
     for i in range(len(nums)):
         valor_mas_bajo_indice = i
         for j in range(i + 1, len(nums)):
-            valor_mas_bajo_indice = j
+            if nums[j] < nums[valor_mas_bajo_indice]:
+                valor_mas_bajo_indice = j
         nums[i],nums[valor_mas_bajo_indice] = nums[valor_mas_bajo_indice], nums[i]
 
 listaNumerosAleatorios = [5,2,1,8,4]
